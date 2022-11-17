8ddce8aa237a871d92ce33ecda8e976ad42f1eeb
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-11-17 18:23:11 +0700

    Creating Function in Typescript

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,34 @@
-console.log('test')
+let greet: Function;
+
+// greet = 'Hello';
+
+greet = () => {
+    console.log('Halo Lagi')
+}
+
+// Optional
+// const add = (a: number, b: number, c?: number|string) => {
+//     console.log(a + b);
+//     console.log(c);
+// }
+
+const add = (a: number, b: number, c: number|string = 10) => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 10, '20')
+
+// Return rype explicitly
+// const minus = (a: number, b: number):number => {
+//     return a + b;
+// }
+
+const minus = (a: number, b: number) => {
+    return a + b;
+}
+
+let result = minus(10, 7);
+
+// Errors
+// result = 'something'
